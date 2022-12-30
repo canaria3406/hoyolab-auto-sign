5fe95b39e23594fd1b1ab8138bbef7cf5cd6ca2e
canaria3406 <27627468+canaria3406@users.noreply.github.com>
2022-12-30 12:54:47 +0800

    Create getToken.js

---
+++ src/getToken.js
@@ -0,0 +1,10 @@
+/** 進入hoyolab簽到頁面，按F12進入console，貼上以下程式碼後執行即可取得token **/
+/** https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481 **/
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
+
+console.log('ltoken='+getCookie('ltoken')+'; ltuid='+getCookie('ltuid')+';');
