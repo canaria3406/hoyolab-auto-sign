bb56a3dd06bf748110909c23029983525e9bef86
NatsumeAoii <149252113+NatsumeAoii@users.noreply.github.com>
2023-11-30 00:29:40 +0700

    Update getToken.js

--- src/getToken.js
+++ src/getToken.js
@@ -1,21 +1,23 @@
-/** Enter the hoyolab check-in page, press F12 to enter the console, paste the following code and run it to get the token. **/
-/** https://www.hoyolab.com/circles **/
+/**
+ * This function retrieves the token from the hoyolab check-in page.
+ * To use this function, open the hoyolab check-in page, press F12 to enter the console, paste the code, and run it.
+ * Visit: https://www.hoyolab.com/circles
+ */
 
 function getCookie(name) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(';').shift();
 }
+
 let token = 'Error';
+
 if (document.cookie.includes('ltoken=')) {
   token = `ltoken=${getCookie('ltoken')}; ltuid=${getCookie('ltuid')};`;
 } else if (document.cookie.includes('ltoken_v2=')) {
   token = `account_mid_v2=${getCookie('account_mid_v2')}; account_id_v2=${getCookie('account_id_v2')}; ltoken_v2=${getCookie('ltoken_v2')}; ltmid_v2=${getCookie('ltmid_v2')}; ltuid_v2=${getCookie('ltuid_v2')};`;
 }
+
 let ask = confirm(token + '\n\nPress enter, then paste the token into your Google Apps Script Project');
-if (ask) {
-  copy(token);
-  msg = token;
-} else {
-  msg = 'Cancel';
-}
+
+let msg = ask ? token : 'Cancel';
